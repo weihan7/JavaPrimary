4b348666fb6116a4a38588850b7acd3c974de35b
chuweihan <18810499157@163.com>
2019-08-11 21:45:38 +0800

    update: 两数之和添加新解法

--- src/com/leetcode/base/two_sum/Solution.java
+++ src/com/leetcode/base/two_sum/Solution.java
@@ -5,12 +5,13 @@ import java.util.Map;
 
 /**
  * 两数之和
+ * 
  * @author cwh
- *
- *如果出现{2,2,3,4,5}这样的数组，以下解法会有问题。
+ * 
  */
 public class Solution {
 
+	// 方法一：两遍哈希表。 如果出现{2,2,3,4,5}这样的数组，该解法会有问题。
 	public int[] twoSum(int[] nums, int target) {
 		if (nums.length < 2) {
 			return null;
@@ -22,12 +23,24 @@ public class Solution {
 		}
 		for (int i = 0; i < nums.length; i++) {
 			int gap = target - nums[i];
-			if(map.get(gap) !=null && (int)map.get(gap)!=i){//确保第二个索引存在，且不是自己
+			if (map.get(gap) != null && (int) map.get(gap) != i) {// 确保第二个索引存在，且不是自己
 				res[0] = i;
-				res[1] = (int)map.get(gap);
+				res[1] = (int) map.get(gap);
 				break;
 			}
 		}
 		return res;
+	}
+
+	// 方法二：暴力法
+	public int[] twoSum2(int[] nums, int target) {
+		for (int i = 0; i < nums.length; i++) {
+			for (int j = i + 1; j < nums.length; j++) {
+				if (nums[j] == target - nums[i]) {
+					return new int[] { i, j };
+				}
+			}
+		}
+		throw new IllegalArgumentException("No two sum solution");
 	}
 }
